37b5820b68bfa2383ef7acd9b5f52de5e3d7e2c4
chean <tokumshy@cvut.cz>
2023-12-27 19:22:26 +0300

    changed app properties and schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,34 +3,42 @@ create table if not exists users(
     password varchar(500) not null,
     enabled boolean not null,
     real_name varchar(20)
-);
+    );
 
 create table if not exists authorities (
     username varchar(50) not null,
     authority varchar(50) not null,
     constraint fk_authorities_users foreign key(username) references users(username)
-);
+    );
 create unique index if not exists ix_auth_username on authorities (username,authority);
 
 create table if not exists clients(
-    id bigserial primary key,
-    name varchar (256) not null,
+                                      id bigserial primary key,
+                                      name varchar (256) not null,
     surname varchar(256) not null
 
-);
+    );
+
 create table if not exists orders(
-    id_orders bigserial primary key ,
-    total_cost bigint,
-    client_id_orders bigint references clients
+                                     id_orders bigserial primary key ,
+                                     total_cost bigint,
+                                     client_id_orders bigint references clients
 );
 create table if not exists menu(
-    id_menu bigserial primary key,
-    name_menu varchar(256),
+                                   id_menu bigserial primary key,
+                                   name_menu varchar(256),
     price bigint
 
-);
+    );
 create table if not exists menu_and_orders(
-    orders_id_menu bigint references orders,
-    menu_id_orders bigint references menu,
-    primary key(orders_id_menu,menu_id_orders)
-);
+                                              orders_id_menu bigint references orders,
+                                              menu_id_orders bigint references menu,
+                                              primary key(orders_id_menu,menu_id_orders)
+    );
+create table if not exists booking(
+                                      id_booking bigserial ,
+                                      time_of_booking time,
+                                      date_of_booking date,
+                                      client_id_booking bigint references clients,
+                                      primary key(id_booking,client_id_booking)
+    );
